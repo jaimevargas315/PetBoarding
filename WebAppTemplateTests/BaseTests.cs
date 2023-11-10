4c353d6b2ddd8f739cab5bd1b7ea122ce74052c1
Ryan Muller <ryanmuller@websurfingstudios.com>
2023-11-09 22:16:40 -0500

    - Add Initial Project

---
+++ WebAppTemplateTests/BaseTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace WebAppTemplateTests
+{
+    [TestFixture]
+    public class BaseTests
+    {
+        [Test]
+        public void BaseTest()
+        {
+            Assert.IsTrue(1==1);
+        }
+
+    }
+}
